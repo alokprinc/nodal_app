0110eccd312028edc2e46ad0c3da24d1938d017e
alokprinc <alokprince8699@gmail.com>
2022-06-30 15:19:02 +0530

    m

---
+++ clone/src/components/Header.js
@@ -0,0 +1,239 @@
+import styled from 'styled-components';
+
+const Header = (props) => {
+    return (
+        <Container>
+            <Content>
+                <Logo>
+                    <a href='/home' >
+                        <img src='images/home-logo.svg'  alt=''></img>
+                    </a>
+                </Logo>
+                <Search>
+                    <div>
+                        <input placeholder="Search" type="text" ></input>
+                    </div>
+                    <SearchIcon>
+                        <img src='images/search-icon.svg'></img>
+                    </SearchIcon>
+                </Search>
+                <Nav>
+                    <NavListWrap>
+                        <NavList className='active'>
+                            <a >
+                                <img src='images/nav-home.svg'></img>
+                                <span>Home</span>
+                            </a>
+                        </NavList>
+                        <NavList>
+                            <a href='/home'>
+                                <img src='images/nav-network.svg'></img>
+                                <span>My Networks</span>
+                            </a>
+                        </NavList>
+                        <NavList>
+                            <a href='/home'>
+                                <img src='images/nav-jobs.svg'></img>
+                                <span>Jobs</span>
+                            </a>
+                        </NavList>
+                        <NavList>
+                            <a href='/home'>
+                                <img src='images/nav-messaging.svg'></img>
+                                <span>Messaging</span>
+                            </a>
+                        </NavList>
+                        <NavList>
+                            <a href='/home'>
+                                <img src='images/nav-notifications.svg'></img>
+                                <span>Notifications</span>
+                            </a>
+                        </NavList>
+                        <User>
+                            <a>
+                                <img src='images/user.svg'></img>
+                                <span>
+                                    Me
+                                    <img src='images/down-icon.svg'></img>
+                                </span>
+                            </a>
+                            <SignOut>
+                                <a>
+                                    Sign Out
+                                </a>
+                            </SignOut>
+                        </User>
+                        <Work>
+                            <a>
+                                <img src='images/nav-work.svg'></img>
+                                <span>
+                                    Work
+                                    <img src='images/down-icon.svg'></img>
+                                </span>
+                                
+                            </a>
+                        </Work>
+                    </NavListWrap>
+                </Nav>
+            </Content>
+        </Container>
+    );
+};
+const Container = styled.div`
+    background-color: white;
+    border-bottom: 1px solid rgba(0,0,0,0.08);
+    left: 0;
+    top: 0;
+    padding: 0 24px;
+    position: fixed;
+    width: 100vw;
+    z-index: 100;
+`;
+
+const Content = styled.div`
+    display: flex;
+    align-items: center;
+    margin: 0 auto;
+    min-height: 100%;
+    max-width: 1128px;
+`;
+const Logo = styled.span`
+    margin-right: 8px;
+    font-size: 0px;
+`;
+const Search = styled.div`
+    opacity: 1;
+    flex-grow: 1;
+    position: relative;
+    &>div{
+        width: 280px;
+        input{
+            border: none;
+            background-color: #eef3f8;
+            border-radius: 2px;
+            color: rgba(0,0,0,0.9);
+            box-shadow: none;
+            width: 218px;
+            height: 34px;
+            vertical-align: text-top;
+            border-color: darkgray;
+            font-weight: 400;
+            font-size: 14px;
+            padding: 0 8px 0 40px;
+            line-height: 1.75;
+        }
+    }
+`;
+const SearchIcon = styled.div`
+    width: 40px;
+    position: absolute;
+    display: flex;
+    z-index: 1;
+    top: 10px;
+    left: 7px;
+    margin: 0;
+    pointer-events: none;
+    border-radius: 0 2px 2px 0;
+    /* justify-content: center; */
+    align-items: center;
+`;
+const Nav = styled.nav`
+    margin-left: auto;
+    display: block;
+    @media (max-width:768px){
+        position: fixed;
+        background-color: white;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+    }
+`;
+const NavListWrap = styled.ul`
+    display: flex;
+    flex-wrap: nowrap;
+    list-style-type: none;
+    .active{
+        span:after{
+            content: "";
+            transform: scaleX(1);
+            border-bottom: 2px solid var(--white,#fff);
+            bottom: 0;
+            left: 0;
+            position: absolute;
+            transition: transform 0.2s ease-in-out;
+            width: 100%;
+            border-color: var(--black,rgba(0,0,0,0.9));
+        }
+    }
+`;
+const NavList = styled.li`
+    display: flex;
+    align-items: center;
+    a{
+        align-items: center;
+        background-color: transparent;
+        display: flex;
+        flex-direction: column;
+        font-size: 12px;
+        font-weight: 400;
+        justify-content: center;
+        line-height: 1.5;
+        min-height: 42px;
+        min-width: 80px;
+        position: relative;
+        text-decoration: none;
+    }
+    span{
+        color: rgba(0,0,0,0.6);
+        display: flex;
+        align-items: center;
+    }
+    @media (max-width:768px){
+        min-width: 70px;
+        &:hover,&:active{
+            a{
+                span{
+                    color: rgba(0,0,0,0.9);
+                }
+            }
+        }
+    }
+`;
+const SignOut = styled.div`
+    position: absolute;
+    top: 45px;
+    background: white;
+    border-radius: 0 0 5px 5px;
+    width:100px;
+    height:40px;
+    transition-duration: 167ms;
+    font-size: 16px;
+    text-align: center;
+    display: none;
+    
+`;
+const User = styled(NavList)`
+    a{
+        img{
+            width: 25px;
+            height:25px;
+            border-radius: 50%;
+        }
+        span{
+            display: flex;
+            align-items: center;
+        }
+    }
+
+    &:hover{
+        ${SignOut}{
+            display: flex;
+            align-items: center;
+            justify-content:center ;
+        }
+    }
+`;
+const Work = styled(User)`
+    border-left: 1px solid rgba(0,0,0,0.1);
+`;
+export default Header;
