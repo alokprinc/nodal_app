9e7e56319ad6a8d73c5119e2cce9644619cf4ca2
alokprinc <alokprince8699@gmail.com>
2022-08-16 19:25:14 +0530

    m

--- ndl/src/components/Login.js
+++ ndl/src/components/Login.js
@@ -2,10 +2,11 @@ import React from "react";
 import styled from "styled-components";
 import { connect } from "react-redux";
 import { signInAPI } from "../actions";
-
+import { Navigate } from "react-router-dom";
 const Login = (props) => {
   return (
     <Container>
+      {props.user && <Navigate to={"/home"} />}
       <Nav>
         <a href="/home">
           <img src="/images/login-logo.svg" alt=""></img>
@@ -163,7 +164,9 @@ const Google = styled.button`
 `;
 
 const mapStateToProps = (state) => {
-  return {};
+  return {
+    user: state.userState.user,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => ({
